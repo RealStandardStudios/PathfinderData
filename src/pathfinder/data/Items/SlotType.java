ef05f16603d4eedf272c0366ceab8a12ab6ade2f
Matthew <lololirpro@gmail.com>
2014-10-12 21:10:59 +1300

    Added the SlotType enum, will probably need to add that to each item for ease of inventory

---
+++ src/pathfinder/data/Items/SlotType.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package pathfinder.data.Items;
+
+/**
+ * @author Matthew
+ *
+ */
+public enum SlotType {
+	Head, Torso, Hands, Ring, Waist, Legs, Feet, Weapon, RangedWeapon, Shield, None
+}
